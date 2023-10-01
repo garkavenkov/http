a43ab748ee7f8aac762d13d427576eac9bbffe11
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2023-10-01 15:08:26 +0300

    Refactor Request class. Add validation for POST data

--- src/Request.php
+++ src/Request.php
@@ -2,6 +2,9 @@
 
 namespace Http;
 
+use Http\Validator;
+use Registry\Registry;
+
 class Request
 {
     /**
@@ -31,6 +34,13 @@ class Request
      * @var array
      */
     private $request_headers = [];
+    
+    /**
+     * Validator instance
+     *
+     * @var Validator
+     */
+    public Validator $validator;
 
     /**
      * Constructor
@@ -39,7 +49,8 @@ class Request
     {
         $this->method = $_SERVER['REQUEST_METHOD'];
         $this->type = $_SERVER['HTTP_CONTENT_TYPE'];
-               
+        $this->validator = new Validator();
+
         $this->request_headers = array_intersect_key(
                 $_SERVER,
                 array_flip(
@@ -151,4 +162,45 @@ class Request
         }
     }
 
+    /**
+     * Validate posted data with Validator
+     *
+     * @param array $rules      Validation rules for posted data
+     * @param array $messages   Validation errors
+     * @return array            Return posted data, if passed validation or array with errors
+     */
+    public function validate(array $rules, array $messages = []): array
+    {        
+        $validated = [];
+        $errors = [];
+        
+        foreach($rules as $field => $field_rules) {
+            
+            foreach($field_rules as $field_rule) {
+                
+                [$rule_name, $rule_param] = array_pad(explode(':', $field_rule, 2), 2, '');
+                
+                if ($this->validator->ruleExists($rule_name)) {
+                  
+                    if (call_user_func_array($this->validator->ruleHandler($rule_name), [$this->posted_data[$field], $rule_param])) 
+                    {
+                        $validated[$field] = $this->posted_data[$field];                        
+                    } else {                
+                        $message = (isset($messages) && isset($messages["$field.$rule_name"])) ? $messages["$field.$rule_name"] : '';                        
+                        $errors[$field] =  $this->validator->ruleError(field: $field, rule_name: $rule_name, rule_param: $rule_param, message: $message);
+                        break;
+                    }                    
+                }
+            }            
+        }        
+        if ($errors) {
+            $session = Registry::get('session');
+            $session->set('errors', $errors);
+            $session->set('form_data', $this->posted_data);
+            header('Location: ' . $_SERVER['HTTP_REFERER'], 303);
+            
+        };     
+        return $validated;
+    }
+   
 }
